15d22a95127c9f55457adccaba2e852cff45df1a
Chad Durkin <DurkinDevelopment@gmail.com>
2022-02-15 06:40:41 -0800

    feat: Updated the get currency spot price function to include the appropriate annotations & added the mvp http response handling for a successful request

--- api_app/views.py
+++ api_app/views.py
@@ -2,6 +2,10 @@ from django.views import View
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import json
 from .models import SpotPrice
 from coinbase.wallet.client import Client
@@ -13,17 +17,16 @@ API_SECRET = 'GmqcDXjUvZqczyOOuBbeq5eOdYxrG3mi'
 # Initiate the coinbase client
 client = Client(API_KEY, API_SECRET)
 
-
 # This function is used to retrieve the currency information from the coinbase api based on the tag
-@method_decorator(csrf_exempt, name='dispatch')
-class Currency(View):
-    
+class Currency(APIView):
     # This function is used as a wrapper function for the entry point of the api request
+    @api_view(('GET',))
+    @csrf_exempt
     def get_currency_spot_price(request, currency_pair):
-        print('inside wrapper')
-        # Create a new get_currency_spot_price_request for auditing
+        # TODO: Create a new get_currency_spot_price_request for auditing
+        # Use the coinbase api wrapper function for making a get spot price request
         get_spot_price_response = CoinbaseAPI.get_spot_price(currency_pair)
-        print(get_spot_price_response)
+        return Response(get_spot_price_response, status=status.HTTP_200_OK)
 
 # TODO: This can be abstracted out into it's own file
 # This class if used for any wrapper functions needed for interacting with the coinbase api
