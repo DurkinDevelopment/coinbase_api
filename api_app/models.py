d7dacc15b18530ea57b43c3b6e79caf478885ff7
Chad Durkin <DurkinDevelopment@gmail.com>
2022-02-15 05:43:50 -0800

    feat: Updated the models to include a spot price & spot price request with aligned attributes

--- api_app/models.py
+++ api_app/models.py
@@ -4,5 +4,11 @@ from django.db.models import Model
 # This class represents a currency's spot price at a given timestamp
 class SpotPrice(Model):
     currency = models.CharField(max_length=200)
+    base = models.CharField(max_length=200)
     amount = models.FloatField()
+
+# This class represents a list of spot prices for a given currency pair at a given time
+class SpotPriceRequest(Model):
+    base = models.CharField(max_length=200)
+    spot_prices = []
     timestamp = models.DateField()
