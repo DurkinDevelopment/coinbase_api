b48ba2ed1315dc746d0d5292d3c4acb01649b430
Chad Durkin <DurkinDevelopment@gmail.com>
2022-02-15 07:27:50 -0800

    feat: Added a placeholder test

--- api_app/tests.py
+++ api_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+        # Test that the expected currency pairs return a 200
+        ## Investigate individual tests vs iteratively testing each 
+        def test_get_currency_spot_price_usd
+            response = self.client.get('/USD')
+            self.assertEqual(response.status_code, 200)
+
