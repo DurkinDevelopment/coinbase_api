29ed34853ab87b0f808cc78113f65c6c60459887
Chad Durkin <DurkinDevelopment@gmail.com>
2022-02-16 06:39:56 -0800

    feat: Updated the test use cases for the urls to be passing

---
+++ api_app/tests/test_urls.py
@@ -0,0 +1,13 @@
+from django.urls import reverse, resolve
+from django.urls import path
+
+class TestUrls: 
+    # Validate that each of the path's view names are correct for each url
+    def test_get_health_url(self):  
+        path = reverse('get_health')
+        assert resolve(path).view_name == "get_health"
+    def test_get_currency_spot_price_url(self):  
+        path = reverse('get_currency_spot_price', kwargs={'currency_pair':'USD'})
+        assert resolve(path).view_name == "get_currency_spot_price"
+
+    
